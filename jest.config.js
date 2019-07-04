f2ab3f93b2d6a12e13b619a0fc94f2c281546d5f
Emmanuel <mygitmail@yahoo.com>
2019-07-04 13:41:49 +0100

    ch-[166272294]: Implementtest coverage
    - Implement feedback
    
    [#166272294]

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,29 @@
 
 module.exports = {
-  reporters: ['default', 'jest-junit']
+  reporters: ['default', 'jest-junit'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{js,jsx}',
+    '!<rootDir>/__test__/**/*.(spec|test).{js,jsx}'
+  ],
+  moduleNameMapper: {
+    '.+\\.(css|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@helpers/(.*)$': '<rootDir>/src/helpers/$1',
+    '^@reducers/(.*)$': '<rootDir>/src/reducers/$1',
+    '^@actions/(.*)$': '<rootDir>/src/actions/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+  },
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules',
+    '<rootDir>/src/index.jsx',
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/'
+  ],
+  transform: {
+    '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
 };
