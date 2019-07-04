3738be600ef9ffcac5faaf67f1829a27027e1565
EMacco <emma4real37@gmail.com>
2019-07-04 11:23:48 +0100

    ch-[166272292]: Setup React Router
    - Install react router dom version 4
    - Create dummy routes for Login, Home and Page Not Found
    
    [#166272292]

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
+import Routes from '../routes';
 
 export default function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+  return <Routes />;
 }
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from '@components/views/Home';
+import Login from '@components/views/Login';
+import NotFound from '@components/views/NotFound';
+
+const Routes = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route path="/" component={NotFound} />
+    </Switch>
+  </Router>
+);
+
+export default Routes;
