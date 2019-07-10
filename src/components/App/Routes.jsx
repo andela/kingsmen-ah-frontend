91bc71ba1d382f405570e80fe31d78c9c91ed692
Emmanuel <mygitmail@yahoo.com>
2019-07-10 19:45:03 +0100

    feat-[167165603]: Create Profile Card
    - Set up reuseable functional component
    
    [Delivers #167165603]

--- src/components/App/Routes.jsx
+++ src/components/App/Routes.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from '@components/views/Home';
 import Login from '@components/views/Login';
 import NotFound from '@components/views/NotFound';
+import Profile from '@components/common/profile';
 
 const Routes = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/profile" component={Profile} />
       <Route path="/" component={NotFound} />
     </Switch>
   </Router>
