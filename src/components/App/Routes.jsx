66dfb67fa7b2302ae77561a2b674467920c81c16
GerrardE <ezeugwagerrard@gmail.com>
2019-07-12 09:48:30 +0100

    chore-[167221908]: Set up heroku
    - Update static json and create an error page
    
    [#167221908]

--- src/components/App/Routes.jsx
+++ src/components/App/Routes.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from '@components/views/Home';
 import Login from '@components/views/Login';
-import ErrorPage from '@components/views/ErrorPage';
-import Profile from '@components/common/profile';
-import Suggested from '@components/common/CreateCommentCard';
+import NotFound from '@components/views/NotFound';
 
 const Routes = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/comment" component={Suggested} />
+      <Route exact path="/" component={Home} />=
       <Route exact path="/login" component={Login} />
-      <Route exact path="/profile" component={Profile} />
       <Route path="/" component={ErrorPage} />
     </Switch>
   </Router>
