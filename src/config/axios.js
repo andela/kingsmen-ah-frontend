429c45b9bac2e0c66ff595b1461c5e723ec6b03f
EMacco <emma4real37@gmail.com>
2019-07-16 16:10:22 +0100

    feat-[166272297]: Login Page
    - Validate user email and password
    - Display global error messages
    - Show login modal when login button is clicked
    - Persist login details to redux store
    - Hide modal on successful login
    
    [Finishes #166272297]

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1'
+  baseURL: 'https://kingsmen-ah-backend-staging.herokuapp.com/api/v1'
 });
 
 instance.interceptors.request.use(config => {
