e96eda2396c92fca687005b82d9c3b4beccc4291
Emmanuel <mygitmail@yahoo.com>
2019-07-19 13:16:56 +0100

    feat[166272295]: User Signup
    - user can signup
    
    [Delivers #166272295]

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -13,7 +13,9 @@ import {
 } from "./types";
 
 axios.defaults.baseURL =
-  "https://kingsmen-ah-backend-staging.herokuapp.com/api/v1";
+  "http://localhost:3000/api/v1";
+// axios.defaults.baseURL =
+//   "https://kingsmen-ah-backend-staging.herokuapp.com/api/v1";
 
 export const setAuthToken = token => {
   if (token) {
